c235334d6d99e1d43ca86e8fd557e55b89867520
Tofu <agithor311@gmail.com>
2024-10-25 15:51:29 -0400

    Started middleware to protect api routes based on jwt token

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const registerUser = require("../controllers/user.controller.js");
+const verifyToken = require("../controllers/auth.middleware.js");
 
 router.post("/", registerUser);
+router.get("/profile", verifyToken, (req, res) => {
+  res.json({ message: `Welcome ${req.user.username}!`, user: req.user });
+});
 
 module.exports = router;
