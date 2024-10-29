b19fb0a11664234085967bb776c3d7b17b61604b
Sam H <samuelhussain@live.com>
2024-10-29 17:26:42 -0400

    Basic design for login page

--- mobile/wardrobe_wizard/lib/main.dart
+++ mobile/wardrobe_wizard/lib/main.dart
@@ -53,19 +53,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -104,30 +91,38 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button 2^x times:',
+              'Welcome to Wardrobe Wizard',
+              style: TextStyle(fontSize: 36),
             ),
-            Text(
-              '${_counter == 0 ? 0 : pow(2, _counter)}',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
             ),
             ElevatedButton(
-              onPressed: () {
-                setState(
-                  () {
-                    _counter = 0;
-                  },
-                );
-              },
-              child: const Text('Reset'),
-            )
+              onPressed: () {},
+              child: const Text('Sign In'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Forgot Password'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Don't have an account? Sign Up",
+              ),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
