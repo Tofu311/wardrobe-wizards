c743b963cdd1991cada6cde8635411a5f94259b2
Mark Bruckert <markbruckert1@Gmail.com>
2024-11-02 13:53:46 -0400

    This GOTTA do it

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/clothing', clothingRoutes);
 
