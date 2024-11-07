685f985c950b8b22b1d4fbe943cc5c30b072dcbe
Mark Bruckert <markbruckert1@Gmail.com>
2024-11-07 16:25:43 -0500

    Allow EVERYTHING

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,7 +7,11 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }));
+  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
