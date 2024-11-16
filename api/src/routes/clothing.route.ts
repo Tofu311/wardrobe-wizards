e6246509a9aaae1ff3f2e42b5634c59af8408ae7
Tofu <agithor311@gmail.com>
2024-11-16 14:48:56 -0500

    Created reclassifyClothing endpoint which enables a user to manually reclassify an article of clothing's type

--- api/src/routes/clothing.route.ts
+++ api/src/routes/clothing.route.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { upload } from '../middleware/upload.middleware';
 import { verifyToken } from '../middleware/auth.middleware';
-import { addClothing, generateOutfit, getClosetItems, deleteClothingItem, saveOutfit, deleteOutfit, getOutfits, getClothing } from '../controllers/clothing.controller';
+import { addClothing, generateOutfit, getClosetItems, deleteClothingItem, saveOutfit, deleteOutfit, getOutfits, getClothing, reclassifyClothing } from '../controllers/clothing.controller';
 
 const router = Router();
 
 router.post('/', verifyToken, upload.single('image'), addClothing);
 
 router.get('/', verifyToken, getClothing, getClosetItems);
+
+router.put('/:clothingId/reclassify', verifyToken, reclassifyClothing)
 
 router.get('/outfit', verifyToken, getOutfits);
 
