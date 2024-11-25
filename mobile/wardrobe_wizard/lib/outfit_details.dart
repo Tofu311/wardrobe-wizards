7d934638c0f9fc84871b9ed6a7f9b770b8229e78
Sam H <samuelhussain@live.com>
2024-11-25 00:33:35 -0500

    Added the outfit delete functionality

--- mobile/wardrobe_wizard/lib/outfit_details.dart
+++ mobile/wardrobe_wizard/lib/outfit_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart';
 import 'package:wardrobe_wizard/clothing.dart';
 import 'package:wardrobe_wizard/outfit.dart';
 
@@ -34,10 +36,77 @@ class _OutfitDetailsState extends State<OutfitDetails> {
       }).toList();
     }
 
+    Future<String?> getToken() async {
+      return await const FlutterSecureStorage().read(key: 'auth_token');
+    }
+
+    Future<void> deleteOutfit() async {
+      final String? token = await getToken();
+      final response = await delete(
+        Uri.parse(
+            'https://api.wardrobewizard.fashion/api/clothing/outfit/${widget.outfit.id}'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200 && context.mounted) {
+        Navigator.pop(context, true);
+      } else {
+        throw Exception('Failed to delete outfit');
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete),
+            color: Colors.red,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Delete Outfit'),
+                    content: const Text(
+                        'Are you sure you want to delete this outfit?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () async {
+                          try {
+                            await deleteOutfit();
+                          } catch (error) {
+                            if (context.mounted) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Error: $error'),
+                                ),
+                              );
+                            }
+                          }
+                        },
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          deleteOutfit();
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Delete',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Center(
